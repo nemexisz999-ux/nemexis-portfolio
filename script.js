fb193e54921c8f7993f9cc8a113909e06b5fb7f2
nemexisz999-ux <nemexisz999@gmail.com>
2026-04-05 22:34:11 +0530

    updated backround and ui

--- script.js
+++ script.js
@@ -1,44 +1,5 @@
-gsap.registerPlugin(ScrollTrigger);
-
-// LENIS
-const lenis = new Lenis();
-
-function raf(time) {
-  lenis.raf(time);
-  requestAnimationFrame(raf);
+function scrollToSection(id){
+  document.getElementById(id).scrollIntoView({
+    behavior:"smooth"
+  });
 }
-requestAnimationFrame(raf);
-
-// HERO
-gsap.from(".hero-content", { y: 80, opacity: 0 });
-
-// GRID
-gsap.from(".box", {
-  scrollTrigger: ".grid",
-  y: 50,
-  opacity: 0,
-  stagger: 0.2
-});
-
-// SKILLS
-gsap.from(".skills i", {
-  scrollTrigger: ".skills",
-  scale: 0,
-  opacity: 0,
-  stagger: 0.2
-});
-
-// SLIDER
-let index = 0;
-const slides = document.querySelector(".slides");
-const total = document.querySelectorAll(".slide").length;
-
-document.querySelector(".next").onclick = () => {
-  index = (index + 1) % total;
-  gsap.to(slides, { x: -index * 100 + "%" });
-};
-
-document.querySelector(".prev").onclick = () => {
-  index = (index - 1 + total) % total;
-  gsap.to(slides, { x: -index * 100 + "%" });
-};
